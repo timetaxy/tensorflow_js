ec6ff9e578d3447628faed074c246b780d9fc81f
= <fantasyz7@naver.com>
2021-12-02 17:29:01 +0900

    default learning complete

--- pratice_boston/tf.js
+++ pratice_boston/tf.js
@@ -1,14 +1,20 @@
 `use strict`;
-const tf = require('@tensorflow/tfjs');
+const tf = require('@tensorflow/tfjs-node');
+const util = require('util');
+// const tf = require('@tensorflow/tfjs');
 const data = require('./data1');
 
 //Hi there 👋. Looks like you are running TensorFlow.js in Node.js. To speed things up dramatically, install our node backend, which binds to TensorFlow C++, by running npm i @tensorflow/tfjs-node, or npm i @tensorflow/tfjs-node-gpu if you have CUDA. Then call require('@tensorflow/tfjs-node'); (-gpu suffix for CUDA) at the start of your program. Visit https://github.com/tensorflow/tfjs-node for more details.
 
-// console.log(data.factor)
-// console.log(data.result)
+// console.log(typeof (data.factor[0]));
+// console.log(typeof (data.result[0]));
 
-const dataFactor = data.factor;
-const dataResult = data.Result;
+
+// const dataFactor = tf.tensor(data.factor);
+// const dataResult = tf.tensor(data.Result);
+// console.log(data.factor[0]);
+const dataFactor = tf.tensor(data.factor);
+const dataResult = tf.tensor(data.result);
 
 const x = tf.input({ shape: [13] });
 // const h1 = tf.layers.dense({units:13, activation:'relu'}).apply(x)
@@ -17,17 +23,19 @@ const model = tf.model({ inputs: x, outputs: y });
 const compileParam = { optimizer: tf.train.adam(), loss: tf.losses.meanSquaredError };
 model.compile(compileParam);
 
+// const simpleFitParam = { epochs: 1 };
 const fitParam = {
-    epochs: 3000,
+    epochs: 10,
     callbacks: {
         onEpochEnd:
             function (epoch, logs) {
-                console.log(`epoch:${epoch}, logs:${logs}, rmse:${Math.sqrt(logs.loss)}`);
+                console.log(`epoch:${epoch}, logs:${util.inspect(logs)}, rmse:${Math.sqrt(logs.loss)}`);
             }
     }
 };
-model.fit(dataFactor[1], dataResult[1], fitParam).then(function (result) {
-    let predition = mode.predict(dataFactor[1]);
-    predition.print();
+model.fit(dataFactor, dataResult, fitParam).then(function (result) {
+    // model.fit(dataFactor, dataResult, simpleFitParam).then(function (result) {
+    let prediction = model.predict(dataFactor);
+    prediction.print();
 });
 
